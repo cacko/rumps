e48a9841448a4bcdf52bdd5f7018a827c5985327
cacko <alex@cacko.net>
2022-07-18 21:56:13 +0100

    this is my quickfix branch and i will use to do my quickfixes

--- rumps/__init__.py
+++ rumps/__init__.py
@@ -17,14 +17,14 @@ statusbar application.
 """
 
 __title__ = 'rumps'
-__version__ = '0.4.9'
+__version__ = '0.4.10'
 __author__ = 'Jared Suttles'
 __license__ = 'Modified BSD'
 __copyright__ = 'Copyright 2020 Jared Suttles'
 
 from . import notifications as _notifications
 from .rumps import (separator, debug_mode, alert, application_support, timers, quit_application, timer,
-                    clicked, MenuItem, SliderMenuItem, Timer, Window, App, slider)
+                    clicked, MenuItem, Menu, SliderMenuItem, Timer, Window, App, slider)
 
 notifications = _notifications.on_notification
 notification = _notifications.notify
