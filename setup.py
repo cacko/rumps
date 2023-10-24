7c917d002acd87460e11e628f83dbfd03f597293
cacko <alex@cacko.net>
2023-10-24 05:48:29 +0100

    This Is Why We Don't Push To Production On Fridays

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     package_data={'': ['LICENSE']},
     license='BSD License',
     install_requires=[
-        'pyobjc==9.2',
+        'pyobjc>=9.0.1',
     ],
     extras_require={
         'dev': [
