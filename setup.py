ee4d9297aef40c5ef3352772e742e582c69858cc
cacko <alex@cacko.net>
2023-12-09 12:57:26 +0000

    This bunny should be killed.

--- setup.py
+++ setup.py
@@ -56,10 +56,10 @@ setup(
     package_data={'': ['LICENSE']},
     license='BSD License',
     install_requires=[
-        "pyobjc-core==9.2",
-        'pyobjc-framework-cocoa>=9.2',
-        'pyobjc-framework-applicationservices==9.2',
-        "pyobjc-framework-quartz==9.2"
+        "pyobjc-core==10.1",
+        'pyobjc-framework-cocoa>=10.1',
+        'pyobjc-framework-applicationservices==10.1',
+        "pyobjc-framework-quartz==10.1"
     ],
     extras_require={
         'dev': [
